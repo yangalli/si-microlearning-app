0d7ace9258d7f43f2e9427514289b9221d7f5169
Yan Galli <yangalli@hotmail.com>
2021-08-06 11:05:57 -0300

    feat: add sound for GamePage

--- src/pages/GamePage/index.tsx
+++ src/pages/GamePage/index.tsx
@@ -1,7 +1,7 @@
 import "./styles.css";
-import useCourses from "../../hooks/useCourses";
 import Lottie from "react-lottie";
 import animationData from "../../assets/animations/13689-gameboy-color.json";
+import useSound from "use-sound";
 
 const animationOptions = {
   loop: true,
@@ -13,16 +13,19 @@ const animationOptions = {
 };
 
 function GamePage() {
-  const courses = useCourses();
+  const [play] = useSound(
+    "https://assets.coderrocketfuel.com/pomodoro-times-up.mp3",
+  );
 
   return (
     <div className="game-container">
       <div className="game-section">
-        <div style={{ display: "flex", flexDirection: "column" }}>
+        <div className="game-content">
           <Lottie options={animationOptions} width={300} height={300} />
           <h1>Vamos jogar?</h1>
         </div>
       </div>
+      <button onClick={() => play()}>Boop!</button>
     </div>
   );
 }
